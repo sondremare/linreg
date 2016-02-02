72ae35336b4d334ea711da816c933d99d1617318
SOondre MNÃaere Overskaug <sondremo@gmail.com>
2016-02-02 17:12:47 +0100

    added plot

--- lin_reg.py
+++ lin_reg.py
@@ -1,8 +1,5 @@
-#from numpy import loadtxt, zeros, ones, array, linspace, logspace, mean, std, arange, insert
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from pylab import plot, show, xlabel, ylabel
+import pylab as pl
 
 def prediction(x, weights, bias):
     return (x.dot(weights) + bias).flatten()
@@ -19,31 +16,25 @@ def gradient_descent(x, y, weights, bias, learning_rate, size):
     bias = bias - (learning_rate * bias_deriv.sum())
     return weights, bias
 
-#Load the datasets
-#training_data = np.loadtxt('data-train.csv', delimiter=',')
 training_data = np.loadtxt('data-train.csv', delimiter=',')
 test_data = np.loadtxt('data-test.csv', delimiter=',')
 
-#Splitting training data into input vector x and target output y
 x = training_data[:, :2]
 y = training_data[:, 2]
 
 size = y.size
-#Adding intercept term
-intercept_terms = np.ones(shape=(size,1))
-#x = np.append(intercept_terms, x, axis=1)
-
 iterations = 100
 learning_rate = 0.01
-
 weights = np.zeros(shape=(2,1)).flatten()
-bias = 0;
+bias = 0
 
+error_array = []
 for i in range(iterations):
     weights, bias = gradient_descent(x, y, weights, bias, learning_rate, size)
     error = mean_squared_error(x,y,weights,bias,size)
-    print("e",error)
-    #print("w",weights)
-    #print("b",bias)
+    error_array.append(error)
 
-#print(weights)
+pl.plot(pl.arange(iterations), error_array)
+pl.xlabel('Iterations')
+pl.ylabel('Mean Squared Error')
+pl.show()
